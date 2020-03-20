ccea5eecbe2772eedb6d547b5fedbcf46ac958fc
jacob kleiman <jacobakleiman@gmail.com>
2020-03-19 17:49:58 -0700

    gitignore and requirements.txt

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jacobakleiman/sei/projects/project_2/project-Pikatsume/.env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
