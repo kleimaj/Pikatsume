7035ed1a37b959d66c0122dbcf32c6b580601315
catcarbonell <hello@catcarbn.com>
2020-03-22 13:15:55 -0700

    Still wrestling with the hamburger menu position. Needs to be inline with lightning bolt icon

--- pikatsume/pikatsume/main_app/static/js/interactions.js
+++ pikatsume/pikatsume/main_app/static/js/interactions.js
@@ -1,1 +1,11 @@
-console.log('pikapika');
+$(document).ready(function() {
+    console.log('ready!')
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+        console.log('clicky')
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $("#navMenu").toggleClass("is-active");
+  
+    });
+  });
