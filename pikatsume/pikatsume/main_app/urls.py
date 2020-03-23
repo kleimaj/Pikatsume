31e8a700ee0a416322fcb2513b69f721946dcb5e
catcarbonell <hello@catcarbn.com>
2020-03-23 09:38:59 -0700

    updating local files

--- pikatsume/pikatsume/main_app/urls.py
+++ pikatsume/pikatsume/main_app/urls.py
@@ -4,13 +4,18 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup', views.signup, name='signup'),
+    
     path('about/', views.about, name='about'),
+    
     path('pikabase/', views.pikabase_index, name='index'),
     path('pikabase/new/', views.new_pika, name='new_pika'),
+    
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.profile_edit, name='profile_form'), 
-    path('accounts/signup', views.signup, name='signup'),
+
     path('catch/', views.catch, name='catch'),
     path('catch/confirm', views.catch_confirm, name='catch_confirm'),
     path('caught/', views.caught, name='caught'),
