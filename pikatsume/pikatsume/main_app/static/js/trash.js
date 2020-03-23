060b8bb2dba5da6ac8926f1a1540edf4773b4dbc
jacob kleiman <jacobakleiman@gmail.com>
2020-03-23 14:49:52 -0700

    trashchu progress

--- pikatsume/pikatsume/main_app/static/js/trash.js
+++ pikatsume/pikatsume/main_app/static/js/trash.js
@@ -1,5 +1,5 @@
 console.log('i am the trash man');
-let pikas = $('.pika_img');
+let pikas = $('.trashchu');
 const trash = `<i class="fas fa-trash-alt"></i>`
 for (p of pikas) {
     console.log(p);
@@ -7,12 +7,12 @@ for (p of pikas) {
         // opaque pika image
         p.style.opacity=.4;
         // let el = document.createElement(trash);
-        p.insertAdjacentHTML('afterend', '<i class="fas fa-trash-alt fa-9x" id="trash"></i>');
+        p.insertAdjacentHTML('afterbegin', '<i class="fas fa-trash-alt fa-7x" id="trash"></i>');
         // add trash can over image
     });
     p.addEventListener("mouseout", function() {
         p.style.opacity=1;
-        p.nextSibling.remove()
+        $('#trash').remove()
     })
     // , function() {
     //     p.style.opacity=1
