a7c1b444625a30a4f925bbae2b7cbc7c9b254b5e
jacob kleiman <jacobakleiman@gmail.com>
2020-03-23 21:04:48 -0700

    trash is trash

--- pikatsume/pikatsume/main_app/static/js/trash.js
+++ pikatsume/pikatsume/main_app/static/js/trash.js
@@ -1,18 +1,21 @@
 console.log('i am the trash man');
 let pikas = $('.trashchu');
-const trash = `<i class="fas fa-trash-alt"></i>`
+// const trash = `<i class="fas fa-trash-alt"></i>`
+let currentCard = '';
+
 for (p of pikas) {
     console.log(p);
-    p.addEventListener("mouseover",function(){
+    p.addEventListener("mouseover",function(event){
         // opaque pika image
         p.style.opacity=.4;
+        currentcard = p
         // let el = document.createElement(trash);
-        p.insertAdjacentHTML('afterbegin', '<i class="fas fa-trash-alt fa-7x" id="trash"></i>');
-        // add trash can over image
+        p.insertAdjacentHTML('afterend', '<i class="fas fa-trash-alt fa-7x" id="trash"></i>');
+        let trash = document.querySelector("#trash");
     });
     p.addEventListener("mouseout", function() {
         p.style.opacity=1;
-        $('#trash').remove()
+        $('#trash').remove();
     })
     // , function() {
     //     p.style.opacity=1
